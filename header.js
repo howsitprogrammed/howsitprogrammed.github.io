f7b02dc78396d19c6a9719f0f69fb6a054f9eec8
Merover <meroverandds02@gmail.com>
2021-09-06 20:46:24 +0200

    lmao

---
+++ header.js
@@ -0,0 +1,12 @@
+const header = `
+<header>
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="tutorials.html">Tutorials</a></li>
+        <li><a href="contribute.html">Contribute</a></li>
+        <li><a href="contact.html">Contact</a></li>
+    </ul>
+</header>`
+
+const headerTag = document.getElementById('header');
+headerTag.innerHTML = header;
